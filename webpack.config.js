b3ebfa7b6f2373f7f6e12a8fce411525f6aa3f4c
evan bechtol <evanbechtol@gmail.com>
2022-03-24 10:14:22 -0500

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa");
+const StandaloneSingleSpaPlugin = require("standalone-single-spa-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
@@ -12,14 +13,6 @@ module.exports = (webpackConfigEnv, argv) => {
     disableHtmlGeneration: true,
   });
 
-  const standalonePlugin = defaultConfig.plugins.find(
-    (p) => p.constructor.name === "StandaloneSingleSpaPlugin"
-  );
-
-  standalonePlugin.options.importMapUrl = new URL(
-    "https://react.microfrontends.app/importmap.json"
-  );
-
   const externals = [/^rxjs\/?.*$/];
 
   if (webpackConfigEnv.standalone) {
@@ -29,5 +22,46 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // customizations go here
     externals,
+    plugins: [
+      new HtmlWebpackPlugin(),
+
+      new StandaloneSingleSpaPlugin({
+        // required
+        appOrParcelName: "mfe1",
+
+        // optional - strongly encouraged for single-spa applications
+        activeWhen: ["/route-prefix"],
+
+        // optional - useful for enabling cross-microfrontend imports
+        importMapUrl: new URL(
+          "https://react.microfrontends.app/importmap.json"
+        ),
+
+        // optional - the standalone plugin relies on optionalDependencies in the
+        // package.json. If this doesn't work for you, pass in your HtmlWebpackPlugin
+        // to ensure the correct one is being referenced
+        HtmlWebpackPlugin,
+
+        // optional - defaults to true - turns on or off import-map-overrides.
+        importMapOverrides: true,
+
+        // optional - defaults to null. This allows you to hide the import-map-overrides UI
+        // unless a local storage key is set. See more info at https://github.com/joeldenning/import-map-overrides/blob/master/docs/ui.md#enabling-the-ui
+        importMapOverridesLocalStorageKey: null,
+
+        // optional - defaults to {}. The single-spa custom props passed to the application
+        // Note that these props are stringified into the HTML file
+        customProps: {
+          authToken: "sadf7889fds8u70df9s8fsd",
+        },
+
+        // optional - defaults to turning urlRerouteOnly on
+        // The options object passed into single-spa's start() function.
+        // See https://single-spa.js.org/docs/api#start
+        startOptions: {
+          urlRerouteOnly: true,
+        },
+      }),
+    ],
   });
 };
